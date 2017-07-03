633f2bfd6ccfc9d11bca38749e65d4c8e54e00de
kexiaojiu <kexiaojiu@163.com>
2017-07-03 23:29:43 +0800

    reconstitue the code in game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -2,25 +2,35 @@
 import sys
 import pygame
 
+def check_key_down_events(event, ship):
+    """响应按键"""
+    if event.key == pygame.K_RIGHT:
+        #向右移动飞船
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        #向左移动飞船
+        ship.moving_left = True
+
+
+def check_key_up_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        #停止移动飞船
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        #停止移动飞船
+        ship.moving_left = False  
+
+
 def check_events(ship):
     """监视键盘和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                #向右移动飞船
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                #向左移动飞船
-                ship.moving_left = True
+            check_key_down_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                #停止移动飞船
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                #停止移动飞船
-                ship.moving_left = False             
+            check_key_up_events(event, ship)          
+
 
 def update_screen(ai_settings, screen, ship):
     """更新屏幕的图像，并切换到新屏幕"""    
